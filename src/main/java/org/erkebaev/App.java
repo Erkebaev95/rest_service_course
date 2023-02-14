c52d87bfb45572944a6f26fadf8e5594966480a7
Erkebaev Nursultan <73390365+Erkebaev95@users.noreply.github.com>
2023-02-14 09:34:25 +0600

    work with RestTemplate class

--- src/main/java/org/erkebaev/App.java
+++ src/main/java/org/erkebaev/App.java
@@ -1,13 +1,17 @@
 package org.erkebaev;
 
+import org.springframework.web.client.RestTemplate;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url = "https://reqres.in/api/users/2";
+        String response = restTemplate.getForObject(url, String.class);
+
+        System.out.println(response);
     }
 }
