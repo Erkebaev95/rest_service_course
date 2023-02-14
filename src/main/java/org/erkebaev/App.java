f8e29b28b5ab6d67299501ae3b7c7bf9977eba73
Erkebaev Nursultan <73390365+Erkebaev95@users.noreply.github.com>
2023-02-14 09:42:45 +0600

    work with post request

--- src/main/java/org/erkebaev/App.java
+++ src/main/java/org/erkebaev/App.java
@@ -1,6 +1,10 @@
 package org.erkebaev;
 
+import org.springframework.http.HttpEntity;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Hello world!
@@ -9,8 +13,18 @@ public class App {
     public static void main(String[] args) {
         RestTemplate restTemplate = new RestTemplate();
 
-        String url = "https://reqres.in/api/users/2";
-        String response = restTemplate.getForObject(url, String.class);
+
+        // get
+        /*String url = "https://reqres.in/api/users/2";
+        String response = restTemplate.getForObject(url, String.class);*/
+
+        // post
+        Map<String, String> jsonToSend = new HashMap<>();
+        jsonToSend.put("name","Test name");
+        jsonToSend.put("job","Test job");
+        String url = "https://reqres.in/api/users";
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(jsonToSend);
+        String response = restTemplate.postForObject(url, request, String.class);
 
         System.out.println(response);
     }
